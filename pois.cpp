f2c422a182f3e800c7b89b133fd5616d641cb4d0
Maria Tzevelekou <67122645+beepboop141@users.noreply.github.com>
2023-05-27 15:40:17 +0300

    Update pois.cpp

--- pois.cpp
+++ pois.cpp
@@ -1,60 +1,51 @@
-#include <iostream>  
-#include <vector>  
-#include <iomanip>  
-#include <string>  
-#include <unordered_map>  
-#include <cmath>  
-#include <algorithm>  
-using namespace std;   
-  
-struct point {  
-  double x;   
-  double y;   
-  string category;   
-};  
-  
-double distance(double x1, double y1, double x2, double y2){  
-  double dx = x1-x2;   
-  double dy = y1-y2;   
-  return pow(dx*dx + dy*dy, 0.5);   
-}  
-bool alphab(const pair<string, vector<double>> &a, const pair<string, vector<double>> &b){  
-  return a.first < b.first;   
-}  
-  
-int main() {  
-  int n;   
-  cin >> n;   
-  vector <point> pt(n);   
-  unordered_map<string, vector<double>> distances;   
-  
-  for (int i=0; i<n; i++){  
-   cin >> pt[i].x >> pt[i].y >> pt[i].category;  
-  }  
-  
-  int k;   
-  cin >> k;   
-  vector<double> xtrack(k), ytrack(k);   
-  for (int i =0; i < k; ++i) {  
-    cin >> xtrack[i] >> ytrack[i];  
-  }  
+    #include <iostream>  
+    #include <vector>  
+    #include <map>  
+    #include <string>  
+    #include <iomanip>  
+    #include <cmath>  
+    #include <set>  
+    using namespace std;   
       
-    for (int i =0; i< k; i++){  
-     cout << int(xtrack[i])<< " " <<int( ytrack[i]) << endl;  
-     distances.clear();    
-     for(const auto &p: pt){  
-       double d = distance(p.x, p.y, xtrack[i], ytrack[i]);  
-       distances[p.category].push_back(d);   
-     }  
-    vector<pair<string, vector<double>>> sorted_d(distances.begin(), distances.end());   
-   sort(sorted_d.begin(), sorted_d.end(), alphab);   
-  
-     for (const auto &t : sorted_d) {   
-      vector<double> vals =t.second;   
-      double minval = *min_element(vals.begin(), vals.end());   
-      cout << "  "<< t.first << " " <<fixed << setprecision(3)<<  minval << endl;  
-     }    
-  }  
-  
-  return 0;  
-}
+    struct point {  
+      double x;   
+      double y;   
+      string p;   
+    };  
+    struct xndy {  
+      double x;   
+      double y;   
+    };  
+      
+    double distance (double x1, double y1, double x2, double y2){  
+      double dx = x1-x2;   
+      double dy = y2-y1;   
+      return pow((dx*dx + dy*dy), 0.5);   
+    }  
+    int main() {  
+      int N;   
+      cin >> N;  
+      vector<point> points(N);   
+      for (int i =0; i<N; ++i)  
+        cin >> points[i].x >> points[i].y >> points[i].p;   
+      int k;   
+      cin >> k;  
+      vector<xndy> interest(k);   
+      for (int i =0; i<k; ++i)  
+        cin >> interest[i].x >> interest[i].y;  
+      map<string, set<double>> saske;  
+     map<string, set<double>> :: iterator q;    
+      for (const auto& it: interest){   
+        cout << int(it.x) << " "<< int(it.y) << endl;    
+        for (const auto& pt: points) {  
+          double d = distance(it.x, it.y, pt.x, pt.y);   
+          saske[pt.p].insert(d);   
+        }  
+        for(const auto &j : saske) {  
+          cout << "  "<< j.first <<" ";  
+          cout<< fixed<< setprecision(3) <<*(j.second.begin())<< endl; 
+        }  
+        saske.clear();   
+      }  
+      return 0;   
+    }  
